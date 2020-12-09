8101772ac218693b6b771ec72cc2e001cedc72fd
XIONIX71 <zeus@DESKTOP-AD5J5F6.localdomain>
2020-12-09 11:55:08 +0100

    Question 1 et 2

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -7,14 +7,24 @@ import org.junit.Test;
 public class TourTest {
 
 	@Test
+	public void testEmpiler_tourEmpty_True(){
+		Disque d1 = new Disque(1);
+		Tour t = new Tour();
+				
+		boolean expected = true;
+		boolean actual = t.empiler(d1);
+		assertEquals(expected, actual);
+	}
+
+	@Test
 	public void Question1() {
 		Tour tourInit = new Tour();
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
-		tourInit.empiler(grand);
-		tourInit.empiler(moyen);
-		tourInit.empiler(petit);
+		assertEquals(true, tourInit.empiler(grand));
+		assertEquals(true, tourInit.empiler(moyen));
+		assertEquals(true, tourInit.empiler(petit));
 
 	}
 
@@ -24,9 +34,10 @@ public class TourTest {
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
-		tourInit.empiler(moyen);
-		tourInit.empiler(grand);
-		tourInit.empiler(petit);
+		assertEquals(true, tourInit.empiler(moyen));
+		assertEquals(false, tourInit.empiler(grand));
+		assertEquals(true, tourInit.empiler(petit));
+		assertEquals(false, tourInit.empiler(petit));
 
 	}
 
