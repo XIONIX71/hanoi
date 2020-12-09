75de93a0be1c62f68844fb860c114b215bfb80ab
XIONIX71 <zeus@DESKTOP-AD5J5F6.localdomain>
2020-12-09 11:09:38 +0100

    test

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -7,8 +7,15 @@ import org.junit.Test;
 public class TourTest {
 
 	@Test
-	public void test() {
+	public void Question1() {
+		Tour tourInit = new Tour();
+		Tour tourInter = new Tour();
+		Tour tourDest = new Tour();
 
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		
 	}
 
 }
