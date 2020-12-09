e72f52e19807d6ac7973d4edc904b7e4f92bdb78
XIONIX71 <zeus@DESKTOP-AD5J5F6.localdomain>
2020-12-09 11:28:17 +0100

    Question 1 qui ne fonctionne pas

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -9,13 +9,25 @@ public class TourTest {
 	@Test
 	public void Question1() {
 		Tour tourInit = new Tour();
-		Tour tourInter = new Tour();
-		Tour tourDest = new Tour();
-
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
-		
+		tourInit.empiler(grand);
+		tourInit.empiler(moyen);
+		tourInit.empiler(petit);
+
+	}
+
+	@Test
+	public void Question1f() {
+		Tour tourInit = new Tour();
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		tourInit.empiler(moyen);
+		tourInit.empiler(grand);
+		tourInit.empiler(petit);
+
 	}
 
 }
